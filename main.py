5b07dd1e8297d2fee96dcf645a8b278a8a6a647a
Jon Ribeiro <contact@jonathas.com>
2022-01-01 12:27:37 +0100

    feat: changing and saving new xml

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 #!/usr/bin/env python
 
-import xml.etree.ElementTree as ET
+from xml.etree import ElementTree
+import json
 
 evdev_path="/usr/share/X11/xkb/rules/evdev.xml"
 
-def parseXML():
- tree = ET.parse(evdev_path)
- root = tree.getroot()
-
- for item in root.findall("./layoutList/layout/configItem"):
-    for child in item:
-        print(child.tag.title())
-        print(child.text)
-
 def main():
-    parseXML()
+    tree = parse_xml()
+    set_flags_in_xml(tree)
+    write_xml(tree)
+
+def parse_xml():
+    parser = ElementTree.XMLParser(target=ElementTree.TreeBuilder(insert_comments=True))
+    return ElementTree.parse(evdev_path, parser = parser)
+
+def set_flags_in_xml(tree):
+    flags = load_flags()
+    root = tree.getroot()
+    for item in root.findall("./layoutList/layout/configItem"):
+        set_flag(item, flags)
+
+def load_flags():
+    f = open("./flags.json")
+    return json.load(f)
+
+def set_flag(item, flags):
+    country_code, language = get_layout_id_from_xml(item)
+    layout = get_layout_info(country_code, language, flags)
+    if len(layout) > 0:
+        item.find("./shortDescription").text = layout[0]["flag"]
+
+def get_layout_id_from_xml(item):
+    country_code = item.find("./name")
+    language = item.find("./shortDescription")
+    if country_code is not None and language is not None:
+        return country_code.text, language.text
+    else:
+        return "", "" 
+
+def get_layout_info(country_code, language, flags):
+    return list(filter(lambda x:(x["code"]==country_code and x["shortDescription"]==language), flags))
+
+def write_xml(tree):
+    tree.write("./evdev.xml", encoding='utf-8', xml_declaration=True)
 
 if __name__ == "__main__":
     main()
